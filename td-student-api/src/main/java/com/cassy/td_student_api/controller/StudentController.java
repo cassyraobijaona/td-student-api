392f8cf57af11d08a5375bdf4164989485a41108
cassy <cassyraobijaona@gmail.com>
2026-03-19 23:40:26 +0300

    Partie ResponseEntity POST /students

--- td-student-api/src/main/java/com/cassy/td_student_api/controller/StudentController.java
+++ td-student-api/src/main/java/com/cassy/td_student_api/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.cassy.td_student_api.controller;
 
 import com.cassy.td_student_api.entity.Student;
 import com.cassy.td_student_api.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +18,17 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public List<Student> addStudents(@RequestBody List<Student> students) {
-        return studentService.addStudents(students);
+    public ResponseEntity<List<Student>> addStudents(@RequestBody List<Student> students) {
+        try {
+            List<Student> allStudents = studentService.addStudents(students);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(allStudents);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
     }
 
     @GetMapping("/students")
