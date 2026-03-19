5de5236bd224064c87cec7fa4116766cac74c098
cassy <cassyraobijaona@gmail.com>
2026-03-19 23:56:30 +0300

    GET /students avec ResponseEntity

--- td-student-api/src/main/java/com/cassy/td_student_api/controller/StudentController.java
+++ td-student-api/src/main/java/com/cassy/td_student_api/controller/StudentController.java
@@ -32,20 +32,40 @@ public class StudentController {
     }
 
     @GetMapping("/students")
-    public String getStudents(@RequestHeader(value = "Accept", required = false) String accept) {
+    public ResponseEntity<?> getStudents(@RequestHeader(value = "Accept", required = false) String accept) {
+        try {
+            if (accept == null) {
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body("Entête 'Accept' manquant");
+            }
 
-        if (accept == null || !accept.equals("text/plain")) {
-            return "Format non supporté";
+            if (!accept.equals("application/json") && !accept.equals("text/plain")) {
+                return ResponseEntity
+                        .status(HttpStatus.NOT_IMPLEMENTED)
+                        .body("Format non supporté");
+            }
+
+            List<Student> students = studentService.getStudents();
+
+            if (accept.equals("text/plain")) {
+                StringBuilder sb = new StringBuilder();
+                for (Student s : students) {
+                    sb.append(s.getFirstName()).append(" ").append(s.getLastName()).append("\n");
+                }
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(sb.toString());
+            }
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(students);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erreur serveur");
         }
-
-        List<Student> students = studentService.getStudents();
-
-        String result = "";
-
-        for (Student s : students) {
-            result += s.getFirstName() + " " + s.getLastName() + "\n";
-        }
-
-        return result;
     }
 }
