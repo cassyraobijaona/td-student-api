dc6b5a835839981fff260ce78cb45feb5e7d8837
cassy <cassyraobijaona@gmail.com>
2026-03-19 09:52:19 +0300

    Initialisation projet Spring Boot

---
+++ td-student-api/src/main/java/com/cassy/td_student_api/TdStudentApiApplication.java
@@ -0,0 +1,14 @@
+package com.cassy.td_student_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdStudentApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TdStudentApiApplication.class, args);
+	}
+
+}
---
+++ td-student-api/src/test/java/com/cassy/td_student_api/TdStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassy.td_student_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
