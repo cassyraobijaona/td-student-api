2595543d52a582526205b8aab3ecc7040e373dd9
cassy <cassyraobijaona@gmail.com>
2026-03-19 23:35:24 +0300

    Gestion ResponseEntity pour /welcome

--- td-student-api/src/main/java/com/cassy/td_student_api/controller/WelcomeController.java
+++ td-student-api/src/main/java/com/cassy/td_student_api/controller/WelcomeController.java
@@ -1,12 +1,23 @@
 package com.cassy.td_student_api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam(required = false) String name) {
+
+        if (name == null || name.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Paramètre 'name' manquant");
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Welcome " + name);
     }
 }
